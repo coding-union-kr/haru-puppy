59e25507cd11f0a96093272c443471f4ed0535ca
harper <35358294+yooyouny@users.noreply.github.com>
2023-12-08 20:27:17 +0900

    style: Google Java Format 적용 #5

--- back/src/main/java/com/developaw/harupuppy/HarupuppyApplication.java
+++ back/src/main/java/com/developaw/harupuppy/HarupuppyApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HarupuppyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HarupuppyApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(HarupuppyApplication.class, args);
+  }
 }
