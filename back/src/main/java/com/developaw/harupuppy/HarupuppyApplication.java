9e326d5d555db30d9eb88e3ded29a9b4f06611d8
senique-dev <minirose42@gmail.com>
2023-12-08 19:11:15 +0900

    chore: 백엔드 init #5 - .gitignore 생성 - DB관련 환경변수 설정 - 주요 디펜던시 추가

---
+++ back/src/main/java/com/developaw/harupuppy/HarupuppyApplication.java
@@ -0,0 +1,13 @@
+package com.developaw.harupuppy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarupuppyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarupuppyApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/developaw/harupuppy/HarupuppyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developaw.harupuppy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarupuppyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
