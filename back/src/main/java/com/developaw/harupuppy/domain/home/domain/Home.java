66cf1ac84769420a6c62f91368ac7b1c61024fa3
senique-dev <minirose42@gmail.com>
2023-12-10 19:03:52 +0900

    feat: 홈 엔티티 구현 #9

---
+++ back/src/main/java/com/developaw/harupuppy/domain/home/domain/Home.java
@@ -0,0 +1,24 @@
+package com.developaw.harupuppy.domain.home.domain;
+
+import com.developaw.harupuppy.domain.dog.domain.Dog;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "home")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Home {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "home_id", updatable = false, nullable = false)
+    private Long homeId;
+
+    @Column(name = "home_name", nullable = false)
+    private String homeName;
+
+    @OneToOne(mappedBy = "home_id")
+    private Dog dog;
+}
