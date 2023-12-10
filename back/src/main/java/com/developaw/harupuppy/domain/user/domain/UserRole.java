15ebd1f157d3e98a65f5ee613b7eec9e6661c4d4
senique-dev <minirose42@gmail.com>
2023-12-10 21:55:19 +0900

    chore: UserRole enum 클래스 구성

--- back/src/main/java/com/developaw/harupuppy/domain/user/domain/UserRole.java
+++ back/src/main/java/com/developaw/harupuppy/domain/user/domain/UserRole.java
@@ -5,4 +5,14 @@ import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-public enum UserRole {}
+public enum UserRole {
+  DAD("아빠"),
+  MOM("엄마"),
+  UNNI("언니"),
+  OPPA("오빠"),
+  NUNA("누나"),
+  HYEONG("형"),
+  YOUNGER("동생");
+
+  private final String desc;
+}
