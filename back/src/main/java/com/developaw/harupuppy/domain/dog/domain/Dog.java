ef8491551495226731ead623fe0026f29ca2460d
senique-dev <minirose42@gmail.com>
2023-12-12 02:50:42 +0900

    chore: Dog클래스 birthday 자료형변경 및 불필요하거나 겹치는부분 삭제

--- back/src/main/java/com/developaw/harupuppy/domain/dog/domain/Dog.java
+++ back/src/main/java/com/developaw/harupuppy/domain/dog/domain/Dog.java
@@ -3,13 +3,12 @@ package com.developaw.harupuppy.domain.dog.domain;
 import com.developaw.harupuppy.domain.home.domain.Home;
 import com.developaw.harupuppy.domain.user.domain.User;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.Size;
+import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,45 +17,34 @@ import java.util.List;
 @Getter
 @Table(name = "DOG")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NonNull
 public class Dog {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "dog_id", nullable = false, updatable = false)
   private Long dogId;
 
-  @Column(name = "name", nullable = false)
+  @Column
+  @Size(min = 1, max = 5)
   private String name;
 
   @Column(name = "img")
   private String profilePicture;
 
   @Enumerated(EnumType.STRING)
-  @Column(name = "gender")
   private DogGender gender;
 
-  @Column(name = "birthday")
-  private LocalDateTime birthday;
+  private LocalDate birthday;
 
-  @Column(name = "weight")
   private double weight;
 
-  @CreatedDate
-  @Column(name = "created_date", updatable = false)
-  private LocalDateTime createdDate;
-
-  @LastModifiedDate
-  @Column(name = "modified_date")
-  private LocalDateTime modifiedDate;
 
   @OneToOne
   @JoinColumn(name = "home_id")
   private Home home;
 
-//  @OneToMany(mappedBy = "dog")
-//  private List<User> users = new ArrayList<User>();
 
   @Builder
-  public Dog (String name, String profilePicture, DogGender gender, LocalDateTime birthday, double weight){
+  public Dog (String name, String profilePicture, DogGender gender, LocalDate birthday, double weight){
     this.name = name;
     this.profilePicture = profilePicture;
     this.gender = gender;
