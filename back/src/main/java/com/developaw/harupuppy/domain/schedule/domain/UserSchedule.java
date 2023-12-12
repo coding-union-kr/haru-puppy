d01a1300d93b1ffc398617f3a54f5493829362f5
harper <35358294+yooyouny@users.noreply.github.com>
2023-12-12 23:09:48 +0900

    feat: 유저와 스케줄의 연관테이블인 유저스케줄 정의 #8 - 복합키로 구성

---
+++ back/src/main/java/com/developaw/harupuppy/domain/schedule/domain/UserSchedule.java
@@ -0,0 +1,17 @@
+package com.developaw.harupuppy.domain.schedule.domain;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "USER_SCHEDULES")
+public class UserSchedule {
+
+  @EmbeddedId private UserSchedulePK userSchedulePK;
+}
