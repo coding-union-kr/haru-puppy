e1ef4894499414b315e592d2cf4d74bfa0e79d3c
harper <35358294+yooyouny@users.noreply.github.com>
2023-12-15 06:48:40 +0900

    fix: dto 생성자 부분 삭제 #18

--- back/src/main/java/com/developaw/harupuppy/domain/schedule/dto/ScheduleCreateDto.java
+++ back/src/main/java/com/developaw/harupuppy/domain/schedule/dto/ScheduleCreateDto.java
@@ -12,10 +12,8 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 public record ScheduleCreateDto(
-
         @NotNull(message = "스케줄 타입 지정이 필요합니다") ScheduleType scheduleType,
         @NotNull(message = "메이트 지정이 필요합니다") List<UserScheduleDto> mates,
         @NotBlank(message = "스케줄 날짜 지정이 필요합니다") String scheduleDate,
@@ -25,15 +23,8 @@ public record ScheduleCreateDto(
         String memo
 ) {
 
-    public ScheduleCreateDto {
-        validateDateTime(scheduleDate, scheduleTime);
-        Objects.requireNonNull(scheduleType, "스케줄 타입은 필수값 입니다");
-        Objects.requireNonNull(mates, "메이트는 필수값 입니다");
-        Objects.requireNonNull(scheduleDate, "스케줄 날짜는 필수값 입니다");
-        Objects.requireNonNull(scheduleTime, "스케줄 시간은 필수값 입니다");
-    }
-
     public static Schedule fromDto(ScheduleCreateDto dto) {
+        validateDateTime(dto.scheduleDate, dto.scheduleTime);
         return Schedule.builder()
                 .scheduleDateTime(Schedule.parseDateTime(dto.scheduleDate(), dto.scheduleTime()))
                 .scheduleType(dto.scheduleType())
@@ -45,14 +36,11 @@ public record ScheduleCreateDto(
     }
 
     private static void validateDateTime(String date, String time) {
-        if (date == null || time == null) {
-            throw new IllegalArgumentException("Date and time must not be null");
-        }
         try {
             LocalDate.parse(date, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
             LocalTime.parse(time, DateTimeFormatter.ofPattern("HH:mm"));
         } catch (Exception e) {
-            throw new IllegalArgumentException("Invalid date or time format", e);
+            throw new IllegalArgumentException("스케줄 날짜와 시간이 유효하지 않습니다", e);
         }
     }
 }
