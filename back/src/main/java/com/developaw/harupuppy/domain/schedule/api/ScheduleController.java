30d404f45f186836ed04925043e286d2daeb042f
harper <35358294+yooyouny@users.noreply.github.com>
2023-12-15 06:46:10 +0900

    chore: validation 의존성 추가 및 활성화 #18

--- back/src/main/java/com/developaw/harupuppy/domain/schedule/api/ScheduleController.java
+++ back/src/main/java/com/developaw/harupuppy/domain/schedule/api/ScheduleController.java
@@ -4,8 +4,8 @@ import com.developaw.harupuppy.domain.schedule.application.ScheduleService;
 import com.developaw.harupuppy.domain.schedule.dto.ScheduleCreateDto;
 import com.developaw.harupuppy.global.common.response.ApiResponse;
 import com.developaw.harupuppy.global.common.response.Response.Status;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +19,7 @@ public class ScheduleController {
 
     //TODO :: SpringContextHolder에 저장된 UserDto를 컨트롤러로 가져와 service로 보내기
     @PostMapping
-    public ApiResponse<Void> create(@RequestBody @Valid ScheduleCreateDto dto) {
+    public ApiResponse<Void> create(@RequestBody @Validated ScheduleCreateDto dto) {
         scheduleService.create(dto);
         return ApiResponse.ok(Status.CREATE, null);
     }
