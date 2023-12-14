87b9b0585544164b971e775fd4def45d9f80ccf1
harper <35358294+yooyouny@users.noreply.github.com>
2023-12-15 05:54:54 +0900

    feat: 스케줄과 연관된 유저스케줄 생성 추가 #18

--- back/src/main/java/com/developaw/harupuppy/domain/schedule/dto/ScheduleCreateDto.java
+++ back/src/main/java/com/developaw/harupuppy/domain/schedule/dto/ScheduleCreateDto.java
@@ -4,25 +4,29 @@ import com.developaw.harupuppy.domain.schedule.domain.AlertType;
 import com.developaw.harupuppy.domain.schedule.domain.RepeatType;
 import com.developaw.harupuppy.domain.schedule.domain.Schedule;
 import com.developaw.harupuppy.domain.schedule.domain.ScheduleType;
-import com.developaw.harupuppy.domain.user.dto.ScheduleUserDto;
+import com.developaw.harupuppy.domain.user.dto.UserScheduleDto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 public record ScheduleCreateDto(
+
         @NotNull(message = "스케줄 타입 지정이 필요합니다") ScheduleType scheduleType,
-        @NotNull(message = "메이트 지정이 필요합니다") List<ScheduleUserDto> mates,
+        @NotNull(message = "메이트 지정이 필요합니다") List<UserScheduleDto> mates,
         @NotBlank(message = "스케줄 날짜 지정이 필요합니다") String scheduleDate,
         @NotBlank(message = "스케줄 시간 지정이 필요합니다") String scheduleTime,
         RepeatType repeatType,
         AlertType alertType,
         String memo
 ) {
+
     public ScheduleCreateDto {
+        validateDateTime(scheduleDate, scheduleTime);
         Objects.requireNonNull(scheduleType, "스케줄 타입은 필수값 입니다");
         Objects.requireNonNull(mates, "메이트는 필수값 입니다");
         Objects.requireNonNull(scheduleDate, "스케줄 날짜는 필수값 입니다");
@@ -30,15 +34,16 @@ public record ScheduleCreateDto(
     }
 
     public static Schedule fromDto(ScheduleCreateDto dto) {
-        validateDateTime(dto.scheduleDate, dto.scheduleTime);
         return Schedule.builder()
                 .scheduleDateTime(Schedule.parseDateTime(dto.scheduleDate(), dto.scheduleTime()))
                 .scheduleType(dto.scheduleType())
+                .mates(new ArrayList<>())
                 .alertType(dto.alertType())
                 .repeatType(dto.repeatType())
                 .memo(dto.memo())
                 .build();
     }
+
     private static void validateDateTime(String date, String time) {
         if (date == null || time == null) {
             throw new IllegalArgumentException("Date and time must not be null");
