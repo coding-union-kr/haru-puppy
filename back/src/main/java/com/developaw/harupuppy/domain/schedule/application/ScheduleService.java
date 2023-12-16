634ca95b6d3ed0002362aae5100a81f4dea9b51b
harper <35358294+yooyouny@users.noreply.github.com>
2023-12-16 17:17:53 +0900

    feat: 반복 스케줄들의 insert 로직 추가 #18 - 처음엔 요청 온 단일 스케줄만 저장 - RepeatType이 있을 경우 타입에 맞는 DateTime들 생성 - 각 DateTime들을 set한 Schedule 리스트 생성 - 각 Schedule과 담당 User들을 set한 UserSchedule 리스트 생성 - saveAll로 저장

--- back/src/main/java/com/developaw/harupuppy/domain/schedule/application/ScheduleService.java
+++ back/src/main/java/com/developaw/harupuppy/domain/schedule/application/ScheduleService.java
@@ -2,6 +2,7 @@ package com.developaw.harupuppy.domain.schedule.application;
 
 import com.developaw.harupuppy.domain.schedule.dao.ScheduleRepository;
 import com.developaw.harupuppy.domain.schedule.dao.UserScheduleRepository;
+import com.developaw.harupuppy.domain.schedule.domain.RepeatType;
 import com.developaw.harupuppy.domain.schedule.domain.Schedule;
 import com.developaw.harupuppy.domain.schedule.domain.UserSchedule;
 import com.developaw.harupuppy.domain.schedule.dto.ScheduleCreateDto;
@@ -10,15 +11,20 @@ import com.developaw.harupuppy.domain.user.dto.UserScheduleDto;
 import com.developaw.harupuppy.domain.user.repository.UserRepository;
 import com.developaw.harupuppy.global.common.exception.CustomException;
 import com.developaw.harupuppy.global.common.response.Response.ErrorCode;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
+@Slf4j
 public class ScheduleService {
     private final ScheduleRepository scheduleRepository;
     private final UserScheduleRepository userScheduleRepository;
@@ -29,12 +35,77 @@ public class ScheduleService {
         List<User> mates = validateMates(dto.mates());
 
         Schedule schedule = ScheduleCreateDto.fromDto(dto);
+
         scheduleRepository.save(schedule);
 
         List<UserSchedule> userSchedules = UserSchedule.of(mates, schedule);
         userSchedules.forEach(schedule::addMate);
 
         userScheduleRepository.saveAll(userSchedules);
+
+        if (dto.repeatType() != null && !RepeatType.NONE.equals(dto.repeatType())) {
+            createRepeatSchedule(schedule, mates);
+        }
+    }
+
+    @Transactional
+    public void createRepeatSchedule(Schedule schedule, List<User> mates) {
+        LocalDateTime startDate = schedule.getScheduleDateTime();
+        LocalDateTime endDate = startDate.plusYears(1).withMonth(12).withDayOfMonth(31);
+
+        String repeatId = UUID.randomUUID().toString();
+        schedule.setRepeatId(repeatId);// 등록 일자의 스케줄에도 repeat id 부여
+
+        log.info("{}, {}", schedule.getRepeatId(), schedule.getRepeatType());
+
+        List<LocalDateTime> dateTimesUntilNextYear = getDateTimesUntilNextYear(schedule.getRepeatType(), startDate,
+                endDate, new ArrayList<>());
+
+        log.info("datetime size : {}", dateTimesUntilNextYear.size());
+
+        List<Schedule> schedules = Schedule.of(dateTimesUntilNextYear, schedule, repeatId);
+        scheduleRepository.saveAll(schedules);
+
+        log.info("schedule size : {}", schedules.size());
+
+        List<UserSchedule> userSchedules = UserSchedule.of(mates, schedules);
+        userSchedules.forEach(schedule::addMate);
+        userScheduleRepository.saveAll(userSchedules);
+
+        log.info("userSchedules size : {}", userSchedules.size());
+    }
+
+    public static List<LocalDateTime> getDateTimesUntilNextYear(RepeatType type, LocalDateTime startDate,
+                                                                LocalDateTime endDate,
+                                                                List<LocalDateTime> repeatedDateTimes) {
+        LocalDateTime current = null;
+        switch (type) {
+            case DAY -> {
+                current = startDate.plusDays(1);
+                while (current.isBefore(endDate)) {
+                    repeatedDateTimes.add(current);
+                    current = current.plusDays(1);
+                }
+            }
+            case WEEK -> {
+                current = startDate.plusWeeks(1);
+                while (current.isBefore(endDate)) {
+                    repeatedDateTimes.add(current);
+                    current = current.plusWeeks(1);
+                }
+            }
+            case MONTH -> {
+                current = startDate.plusMonths(1);
+                while (current.isBefore(endDate)) {
+                    repeatedDateTimes.add(current);
+                    current = current.plusMonths(1);
+                }
+            }
+            case YEAR -> {
+                repeatedDateTimes = List.of(startDate.plusYears(1));
+            }
+        }
+        return repeatedDateTimes;
     }
 
     private List<User> validateMates(List<UserScheduleDto> mates) {
