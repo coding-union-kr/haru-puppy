d805fd9a923bf2ae7f83ecc28d955b3eadc67731
harper <35358294+yooyouny@users.noreply.github.com>
2023-12-20 20:31:45 +0900

    feat: 스케줄 CRUD API 구현 및 공통객체 정의 (#27)
    * deploy: google java 포맷팅 jdk 16이상으로 설정
    
    * feat: 공통 응답 객체 정의 #18
    
    * feat: 공통 커스텀 예외 객체 정의 #18
    
    * feat: 컨트롤러 어드바이스 정의 #18
    
    * feat: 스케줄 생성 관련 dto 정의 및 enum 객체 변환 추가 #18
    
    * feat: 500 에러코드 추가 #18
    
    * feat: 변경된 ERD에 맞도록 엔티티 수정, LocalDateTime 타입으로 변환하는 코드 추가 #18
    
    * feat: 스케줄 생성 API 구현 #18
    
    * style: Google Code Format #18
    
    * chore: 테스트 환경 설정 추가 #18
    
    * feat: 스케줄과 유저스케줄 연관관계 설정 및 관련 메소드 추가 #18
    
    * style: Google Code Format #18
    
    * feat: 스케줄과 연관된 유저스케줄 생성 추가 #18
    
    * chore: validation 의존성 추가 및 활성화 #18
    
    * test: 스케줄 생성 API 테스트 추가 #18
    - controller, service, repository 유닛 테스트 수행
    
    * fix: dto 생성자 부분 삭제 #18
    
    * chore: db ddl-auto 설정을 update로 변경
    
    * test: 정상 테스트를 assertThatNoException로 작성 #18
    
    * fix: @Transactional 추가 #18
    
    * deploy: google java 포맷팅 스텝 삭제
    
    * feat: 반복되는 스케줄들의 그룹 구분을 위한 repeatId 추가 #18
    
    * feat: 반복 스케줄들의 insert 로직 추가 #18
    - 처음엔 요청 온 단일 스케줄만 저장
    - RepeatType이 있을 경우 타입에 맞는 DateTime들 생성
    - 각 DateTime들을 set한 Schedule 리스트 생성
    - 각 Schedule과 담당 User들을 set한 UserSchedule 리스트 생성
    - saveAll로 저장
    
    * test: 반복 스케줄 생성 테스트 작성 #18
    - 23-12-25를 시작으로 일, 주, 월 반복으로 생겨날 스케줄, 유저스케줄의 개수 확인
    
    * feat: 스케줄 수정 API #18
    - modifyDto에 스케줄아이디, 반복아이디, 반복스케줄여부 생성
    - 반복스케줄여부가 true면 반복아이디로 현재 스케줄 이후인 스케줄들을 찾아와 전부변경
    
    * refactor: UserSchedule cascade 설정 변경 #18
    - 스케줄 삭제시 연관된 UserSchedule도 함께 삭제되도록 설정
    
    * style: 컨벤션에 맞도록 dto 이름 변경 #18
    
    * feat: 스케줄 상태변경 API 구현 #18
    
    * feat: 요청객체, 응답객체 수정 #18
    
    * chore: 요청객체 패키지 경로 변경 #18
    
    * fix: 반복객체조회 repository 메소드의 파라미터 수정 #18
    
    * test: 스케줄 수정, 삭제 서비스 테스트 코드 추가 #18
    
    * feat: pathVariable, RequestParam 값 검증 추가 #18
    
    * feat: 스케줄 조회 API #18
    - 단일조회
    - 특정 월 스케줄 조회
    
    * chore: DateTimeFormatter 형식 수정 #18
    - yyyy -> uuuu로 변경
    
    * test: 스케줄 조회 서비스 유닛테스트 작성 #18

---
+++ back/src/main/java/com/developaw/harupuppy/domain/schedule/dto/response/ScheduleResponse.java
@@ -0,0 +1,37 @@
+package com.developaw.harupuppy.domain.schedule.dto.response;
+
+import com.developaw.harupuppy.domain.schedule.domain.AlertType;
+import com.developaw.harupuppy.domain.schedule.domain.RepeatType;
+import com.developaw.harupuppy.domain.schedule.domain.Schedule;
+import com.developaw.harupuppy.domain.schedule.domain.ScheduleType;
+import com.developaw.harupuppy.domain.schedule.domain.UserSchedule;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record ScheduleResponse(
+        Long scheduleId,
+        ScheduleType scheduleType,
+        LocalDateTime scheduleDateTime,
+        List<UserSchedule> mates,
+        String repeatId,
+        RepeatType repeatType,
+        AlertType alertType,
+        String memo,
+        boolean isActive,
+        boolean isDeleted
+) {
+    public static ScheduleResponse of(Schedule schedule) {
+        return new ScheduleResponse(
+                schedule.getId(),
+                schedule.getScheduleType(),
+                schedule.getScheduleDateTime(),
+                schedule.getMates(),
+                schedule.getRepeatId(),
+                schedule.getRepeatType(),
+                schedule.getAlertType(),
+                schedule.getMemo(),
+                schedule.isActive(),
+                schedule.isDeleted()
+        );
+    }
+}
