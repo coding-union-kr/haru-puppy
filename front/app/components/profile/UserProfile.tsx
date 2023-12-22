64faa8f998793c3a6b47bf2cb938f8045fb2fdf9
junmieee <junmieee@gmail.com>
2023-12-22 18:19:30 +0900

    design: Home 페이지 #41

--- front/app/components/profile/UserProfile.tsx
+++ front/app/components/profile/UserProfile.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import Image from 'next/image';
 import CreateOutlinedIcon from '@mui/icons-material/CreateOutlined';
 
-interface IUserProfileProps {
+export interface IUserProfileProps {
     user?: {
         profileImg?: string;
     };
