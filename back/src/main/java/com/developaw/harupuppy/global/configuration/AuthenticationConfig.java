5f2e8f8ce102d88a0b8eade59dee6ae5237544b7
harper <35358294+yooyouny@users.noreply.github.com>
2023-12-29 21:57:19 +0900

    chore: Spring security 설정 #54 - cors 필터 bean으로 변경 - OAuth 처리 API로 분리

--- back/src/main/java/com/developaw/harupuppy/global/configuration/AuthenticationConfig.java
+++ back/src/main/java/com/developaw/harupuppy/global/configuration/AuthenticationConfig.java
@@ -1,35 +1,35 @@
 package com.developaw.harupuppy.global.configuration;
 
-import com.developaw.harupuppy.domain.user.OAuthSuccessHandler;
-import com.developaw.harupuppy.domain.user.application.OAuthUserService;
 import com.developaw.harupuppy.global.configuration.filter.AuthenticationFilter;
-import java.util.List;
+import java.util.Arrays;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.Customizer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
 @Slf4j
+@Configuration
 public class AuthenticationConfig {
-    private final OAuthUserService oAuth2UserService;
-    private final OAuthSuccessHandler oAuthSuccessHandler;
     private final AuthenticationFilter authenticationFilter;
+    private final CorsConfigurationSource corsConfigurationSource;
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().requestMatchers("/api/users/**");
+        return (web) -> web.ignoring().requestMatchers("/api/users/**", "/auth/**");
     }
 
     @Bean
@@ -40,33 +40,32 @@ public class AuthenticationConfig {
     @Bean
     protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .cors(cors -> cors
-                        .configurationSource(request -> {
-                            CorsConfiguration corsConfiguration = new CorsConfiguration();
-                            corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000")); // 프론트엔드 주소
-                            corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-                            corsConfiguration.setAllowedHeaders(List.of("*"));
-                            corsConfiguration.setAllowCredentials(true);
-                            corsConfiguration.setMaxAge(3600L);
-                            return corsConfiguration;
-                        }))
-                .csrf(AbstractHttpConfigurer::disable)
+                .cors(CorsConfigurer -> CorsConfigurer.configurationSource(corsConfigurationSource))
                 .sessionManagement(
                         (sessionManagement) ->
                                 sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeRequests((authorizeRequests) -> authorizeRequests
-                        .requestMatchers("/api/users/**").permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .requestMatchers("/api/users/**", "/auth/**").permitAll()
                         .anyRequest()
                         .authenticated())
-                .oauth2Login(oAuth2LoginConfigurer -> oAuth2LoginConfigurer
-                        .redirectionEndpoint(redirectionEndpointConfig -> redirectionEndpointConfig
-                                .baseUri("/auth/callback/"))
-                        .userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig
-                                .userService(oAuth2UserService))
-                        .successHandler(oAuthSuccessHandler)
-                )
                 .addFilterBefore(authenticationFilter,
                         UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        config.setAllowedMethods(Arrays.asList("HEAD", "POST", "GET", "PATCH", "DELETE", "PUT", "OPTIONS"));
+        config.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        config.setExposedHeaders(Arrays.asList("Access-Token", "Refresh-Token"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
+
 }
