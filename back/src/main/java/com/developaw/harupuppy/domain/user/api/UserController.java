c40a41a23d981389a543507a59a5e186c4bfde76
harper <35358294+yooyouny@users.noreply.github.com>
2023-12-29 23:52:33 +0900

    feat: OAuth 로그인 API 기능 수정 #54 - 토큰 발급 시 refresh token redis 세션에 저장 - redis 설정 추가 및 구현체 등 - oAuthService를 호출하는 파사드 서비스를 호출하는것으로 리팩토링

--- back/src/main/java/com/developaw/harupuppy/domain/user/api/UserController.java
+++ back/src/main/java/com/developaw/harupuppy/domain/user/api/UserController.java
@@ -1,6 +1,5 @@
 package com.developaw.harupuppy.domain.user.api;
 
-import com.developaw.harupuppy.domain.user.application.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
