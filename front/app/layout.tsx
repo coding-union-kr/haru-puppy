a2ad535983fb5b5723a9d1c89052c64ca031e3fa
CosmicLatte009 <ohhm009@gmail.com>
2023-12-29 22:41:27 +0900

    feat: Kakao SDK 로드 및 초기화 #56

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -1,21 +1,32 @@
 "use client"
-
+import Script from 'next/script'
 import { theme } from '@/styles/theme'
 import './globals.css'
 import { ThemeProvider } from 'styled-components'
 import StyledComponentsRegistry from './registry'
 import { Pretendard } from '@/public/fonts/fonts'
 
-
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+    
   return (
     <StyledComponentsRegistry>
-      <html lang="en">
+      <html lang="kr">
+      <head>
+      <Script 
+        src="https://t1.kakaocdn.net/kakao_js_sdk/2.6.0/kakao.min.js"
+        strategy="afterInteractive"
+        onLoad={() => {
+            if (window.Kakao && !window.Kakao.isInitialized()) {
+                window.Kakao.init('281badc9872f0dabb0a6ffdaedda3fe5');
+                console.log('Kakao SDK 로드하고 초기화됨');
+              }
+        }}
+      />
+        </head>
         <body className={Pretendard.className}>
           <ThemeProvider theme={theme}>
             {children}
