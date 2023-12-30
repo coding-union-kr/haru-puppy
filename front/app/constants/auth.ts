c5b2052ec9ce4c364b1903f100be5f1170e37d5f
junmieee <junmieee@gmail.com>
2023-12-30 15:05:26 +0900

    fix: oAuth 요청 url과 auth.ts 파일 수정 #70

--- front/app/constants/auth.ts
+++ front/app/constants/auth.ts
@@ -11,6 +11,6 @@ const KAKAO_REST_API_KEY = process.env.NEXT_PUBLIC_REST_API_KEY;
 
 export const KAKAO_REDIRECT_URL = "http://localhost:3000/auth/callback/kakao"
 
-export const BACKEND_REDIRECT_URL = "http://localhost:3000/auth/callback/kakao"
+export const BACKEND_REDIRECT_URL = "http://localhost:8080/api/auth/login"
 
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_REST_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URL}&response_type=code`;
