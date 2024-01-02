bc0ccafe26df0b1df5220d02d84cac85d3eb8496
harper <35358294+yooyouny@users.noreply.github.com>
2024-01-03 01:07:14 +0900

    인증 필터 구현 및 OAuth2 로그인 기능 구현 (#68)
    * chore: 인증 관련 디펜던시 추가 #54
    - spring security
    - jjwt
    
    * refactor: Date 검증 및 변환 로직을 utils로 분리 #54
    
    * chore: 도메인 컬럼 이름 변경 #54
    - imgUrl로 통일
    
    * feat: 인가코드까지 백에서 처리할때 설정 #54
    - 인가코드는 프론트에서 처리하는 것으로 추후 변경
    
    * chore: OAuth2 및 jwt 설정추가 #54
    
    * feat: 토큰 생성, 만료여부확인, 리졸브 기능 구현 #54
    
    * feat: 토큰 생성, 만료여부확인, 리졸브 기능 구현 #54
    
    * chore: Spring security 설정 #54
    - cors 필터 bean으로 변경
    - OAuth 처리 API로 분리
    
    * feat: OAuth 로그인 기능 구현 #54
    - 기존 유저일 경우 토큰 발급 후 유저 이메일 반환
    - 신규 유저일 경우 유저 이메일만 반환
    
    * fix: 의존성 문제로 passwordEncoder 분리 #54
    
    * chore: 인증 에러코드 추가 #54
    
    * feat: Spring security 유저 구현체 정의 #54
    
    * feat: OAuth 로그인 API 기능 수정 #54
    - 토큰 발급 시 refresh token redis 세션에 저장
    - redis 설정 추가 및 구현체 등
    - oAuthService를 호출하는 파사드 서비스를 호출하는것으로 리팩토링
    
    * fix: dev-back 기준으로 front 변경사항을 원복 #54
    
    * refactor: DateUtils에 LocalDate 파싱도 추가 #54
    
    * fix: 필터 예외 INVALID_TOKEN으로 통일 #54
    
    * fix: refreshToken uuid로 변경 및 accessToken claim을 넣는것으로 수정 #54
    
    * fix: basicAuth 설정 제거 및 반환형 OAuthLoginResponse로 수정 #54
    
    * fix: 토큰을 응답객체에 넣어 반환 하는것으로 수정 #54
    
    * fix: dev-back 브랜치와 동일하게 맞추기 #54
    
    * chore: accessToken, refreshToken 만료주기 변경 #54
    
    * feat: 토큰 생성시 claims에 닉네임 추가 #54
    
    * fix: 유저 구분을 UserId로 변경 #54
    
    * fix: redis 저장 시 key를 토큰으로 저장하도록 변경 #54
    ---------
    
    Co-authored-by: junmieee <junmieee@gmail.com>
    Co-authored-by: CosmicLatte009 <ohhm009@gmail.com>
    Co-authored-by: CosmicLatte <87015026+CosmicLatte009@users.noreply.github.com>
    Co-authored-by: Jem <76501504+junmieee@users.noreply.github.com>

--- back/src/main/java/com/developaw/harupuppy/domain/user/repository/UserRepository.java
+++ back/src/main/java/com/developaw/harupuppy/domain/user/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package com.developaw.harupuppy.domain.user.repository;
 
 import com.developaw.harupuppy.domain.user.domain.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
