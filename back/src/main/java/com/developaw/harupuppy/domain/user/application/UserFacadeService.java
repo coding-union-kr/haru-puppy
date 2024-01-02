5dd61b450bf223aa243447ff812c4202c28a503a
harper <35358294+yooyouny@users.noreply.github.com>
2024-01-03 02:18:06 +0900

    feat: token 발급 방식 변경 #69 - redis에 저장 시 refreshToken을 key로 저장 - jwt생성 시 userId, email, nickname 저장

--- back/src/main/java/com/developaw/harupuppy/domain/user/application/UserFacadeService.java
+++ back/src/main/java/com/developaw/harupuppy/domain/user/application/UserFacadeService.java
@@ -6,6 +6,7 @@ import com.developaw.harupuppy.domain.user.dto.request.UserCreateRequest;
 import com.developaw.harupuppy.domain.user.dto.response.LoginResponse;
 import com.developaw.harupuppy.domain.user.dto.response.OAuthLoginResponse;
 import com.developaw.harupuppy.domain.user.dto.response.UserCreateResponse;
+import com.developaw.harupuppy.domain.user.dto.response.UserDetailResponse;
 import com.developaw.harupuppy.global.utils.JwtTokenUtils;
 import java.time.Duration;
 import lombok.RequiredArgsConstructor;
@@ -23,12 +24,12 @@ public class UserFacadeService {
     private final JwtTokenUtils jwtTokenUtils;
     private final RedisService redisService;
 
+    @Transactional
     public LoginResponse login(String provider, String code) {
         OAuthLoginResponse response = oAuthService.login(provider, code);
         String email = response.email();
         if (response.isAlreadyRegistered()) {
-            TokenDto token = jwtTokenUtils.generateToken(response.registeredUser());
-            redisService.setValue(token.refreshToken(), email, Duration.ofMillis(refreshExpiredTimeMs));
+            TokenDto token = createTokenAndSave(response.registeredUser(), email);
             return new LoginResponse(response, token.accessToken(), token.refreshToken());
         }
         return LoginResponse.of(response);
@@ -38,8 +39,7 @@ public class UserFacadeService {
     public UserCreateResponse create(HomeCreateRequest request) {
         UserCreateResponse userDetail = userService.create(request);
         String email = userDetail.getUserResponse().email();
-        TokenDto token = jwtTokenUtils.generateToken(email);
-        redisService.setValue(email, token.refreshToken(), Duration.ofMillis(refreshExpiredTimeMs));
+        TokenDto token = createTokenAndSave(userDetail.getUserResponse(), email);
         userDetail.setToken(token);
         return userDetail;
     }
@@ -48,9 +48,15 @@ public class UserFacadeService {
     public UserCreateResponse create(UserCreateRequest request, String homeId) {
         UserCreateResponse userDetail = userService.create(request, homeId);
         String email = userDetail.getUserResponse().email();
-        TokenDto token = jwtTokenUtils.generateToken(email);
-        redisService.setValue(email, token.refreshToken(), Duration.ofMillis(refreshExpiredTimeMs));
+        TokenDto token = createTokenAndSave(userDetail.getUserResponse(), email);
         userDetail.setToken(token);
         return userDetail;
+    }
+
+    @Transactional
+    public TokenDto createTokenAndSave(UserDetailResponse response, String email) {
+        TokenDto token = jwtTokenUtils.generateToken(response);
+        redisService.setValue(token.refreshToken(), email, Duration.ofMillis(refreshExpiredTimeMs));
+        return token;
     }
 }
