77074c09a7b7a68fd3cf88d28a29a1c64bd0823b
senique-dev <minirose42@gmail.com>
2024-01-03 21:29:05 +0900

    chore: RequestMapping api 복수형으로 변경, Put api에서 PathVariable-userId 삭제

--- back/src/main/java/com/developaw/harupuppy/domain/user/api/UserController.java
+++ back/src/main/java/com/developaw/harupuppy/domain/user/api/UserController.java
@@ -5,19 +5,23 @@ import com.developaw.harupuppy.domain.user.dto.UserResponse;
 import com.developaw.harupuppy.domain.user.dto.UserUpdateRequest;
 import com.developaw.harupuppy.global.common.response.ApiResponse;
 import com.developaw.harupuppy.global.common.response.Response;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/users")
 @RequiredArgsConstructor
 @Validated
+@Slf4j
 public class UserController {
     private final UserService userService;
 
-    @PutMapping("/profile/{userId}")
-    public ApiResponse<UserResponse> update (@PathVariable Long userId, @RequestBody UserUpdateRequest request){
-        return ApiResponse.ok(Response.Status.UPDATE, userService.updateUserInformation(userId, request));
+    @PutMapping("/profile")
+    public ApiResponse<UserResponse> updateProfile (@RequestBody @Valid UserUpdateRequest request){
+        return ApiResponse.ok(Response.Status.UPDATE, userService.updateUserInformation(request));
     }
 }
