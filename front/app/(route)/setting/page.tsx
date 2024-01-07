cffa3bb84a7f20e7bfeb1120644291a4e717918d
junmieee <junmieee@gmail.com>
2024-01-07 17:50:01 +0900

    feat: 설정 페이지 각 navmenu 함수 분리 #88

--- front/app/(route)/setting/page.tsx
+++ front/app/(route)/setting/page.tsx
@@ -9,13 +9,14 @@ import { useMutation, useQueryClient } from 'react-query'
 import axios from 'axios'
 import ContainerLayout from '@/app/components/layout/layout'
 import TopNavigation from '@/app/components/navigation/TopNavigation'
-import { BottomNavigation } from '@mui/material'
 import { LOCAL_STORAGE_KEYS } from '@/app/constants/auth'
 import { useRouter } from 'next/navigation'
 import Modal from '@/app/components/modal/modal'
+import BottomNavigation from '@/app/components/navigation/BottomNavigation'
 
 
 const UserDummy = {
+    userId: '11111',
     nickname: '이로',
     role: '엄마',
     profileImg: '/svgs/user_profile.svg'
@@ -23,7 +24,8 @@ const UserDummy = {
 
 const page = () => {
     const [isToggled, setIsToggled] = useState(false);
-    const [isLogoutModalOpen, setLogoutIsModalOpen] = useState(false);
+    const [isLogoutModal, setLogoutIsModal] = useState(false);
+    const [isTerminateModal, setTerminateModal] = useState(false);
 
     const queryClient = useQueryClient();
     const router = useRouter();
@@ -56,8 +58,8 @@ const page = () => {
     };
 
 
-    const { mutate } = useMutation(
-        (active: boolean) => fetchNotification(active, localStorage.getItem('access_token')),
+    const { mutate: notification } = useMutation(
+        (active: boolean) => fetchNotification(active, localStorage.getItem(LOCAL_STORAGE_KEYS.ACCESS_TOKEN)),
         {
             onSuccess: () => {
                 queryClient.invalidateQueries('notifications');
@@ -65,21 +67,67 @@ const page = () => {
         }
     );
 
-    //토글 함수
+    //알림 토글 함수
     const handelToggle = (toggled: boolean) => {
         setIsToggled(toggled)
-        mutate(toggled);
+        notification(toggled);
     }
 
-
+    //매이트 초대 함수(/invite로 라우팅)
     const handleMateInvite = () => {
         router.push('/invite')
     }
 
 
-    const toggleModal = () => {
-        setLogoutIsModalOpen(!isLogoutModalOpen)
+    const toggleLogoutModal = () => {
+        setLogoutIsModal(!isLogoutModal)
     }
+
+    const toggleTerminateModal = () => {
+        setTerminateModal(!isTerminateModal)
+    }
+
+
+    //로그아웃
+    const handelLogout = () => {
+        localStorage.removeItem(LOCAL_STORAGE_KEYS.ACCESS_TOKEN);
+        localStorage.removeItem(LOCAL_STORAGE_KEYS.REFRESH_TOKEN);
+        router.push('/auth/login');
+    }
+
+
+    //회원탈퇴
+    const { mutate: terminateMutation } = useMutation(
+        async ({ userId, accessToken }: { userId: string; accessToken: string | null }) => {
+            const response = await axios.delete(`/api/users/${userId}`, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${accessToken}`,
+                },
+            });
+
+            return response.data;
+        },
+        {
+            onSuccess: (data) => {
+                localStorage.removeItem(LOCAL_STORAGE_KEYS.ACCESS_TOKEN);
+                localStorage.removeItem(LOCAL_STORAGE_KEYS.REFRESH_TOKEN);
+                router.push('/auth/login');
+            },
+            onError: (error) => {
+                console.error('회원 탈퇴 에러:', error);
+            },
+        }
+    );
+
+    //회원 탈퇴 
+    const handleTerminate = () => {
+        const userId = UserDummy.userId;
+        const accessToken = localStorage.getItem('access_token');
+        terminateMutation({ userId, accessToken })
+
+    };
+
 
     return (
         <>
@@ -90,16 +138,24 @@ const page = () => {
                     <NavMenu title='알림 설정' >
                         <ToggleSwitch onToggle={handelToggle} isToggled={isToggled} />
                     </NavMenu>
-                    <NavMenu title='로그아웃' onClick={toggleModal} />
-                    {isLogoutModalOpen && (
+                    <NavMenu title='로그아웃' onClick={toggleLogoutModal} />
+                    {isLogoutModal && (
                         <Modal
                             children="로그아웃하시겠습니까?"
                             btn1="취소"
                             btn2="로그아웃"
-                            onClose={toggleModal}
+                            onClose={toggleLogoutModal}
                         />
                     )}
-                    <NavMenu title='회원 탈퇴' />
+                    <NavMenu title='회원 탈퇴' onClick={toggleTerminateModal} />
+                    {isTerminateModal && (
+                        <Modal
+                            children="정말 탈퇴 하시겠습니까?"
+                            btn1="취소"
+                            btn2="회원 탈퇴"
+                            onClose={toggleTerminateModal}
+                        />
+                    )}
                     <NavMenu title='메이트 초대하기' onClick={handleMateInvite} />
                 </MenuWrapper>
             </Wrapper>
