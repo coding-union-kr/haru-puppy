70ea95894c88bd445b1d7e07f33ffd4c30b26c58
harper <35358294+yooyouny@users.noreply.github.com>
2024-02-02 16:54:19 +0900

    feat: 스케줄 API 변경사항 반영 #110 - 스케줄 수정 시 단일/반복스케줄 유무, 변경 항목에 따라 분기 - replace API 추가로 삭제 후 신규 반복스케줄 추가하는 로직 추가 - delete, replace시 해당 스케줄을 포함해서 진행하도록  repository에 메소드 추가 - UD 작업 시 요청자와 writer가 같은지 확인하는 로직 추가

--- back/src/main/java/com/developaw/harupuppy/domain/schedule/dto/request/ScheduleCreateRequest.java
+++ back/src/main/java/com/developaw/harupuppy/domain/schedule/dto/request/ScheduleCreateRequest.java
@@ -54,7 +54,7 @@ public record ScheduleCreateRequest(
                 .build();
     }
 
-    private static void validateDateTime(String date, String time) {
+    public static void validateDateTime(String date, String time) {
         try {
             LocalDate.parse(date, DateTimeFormatter.ofPattern("uuuu-MM-dd"));
             LocalTime.parse(time, DateTimeFormatter.ofPattern("HH:mm"));
